55905570e1c26f699b83b53c35181d37bc9416a4
Sergey.Kurenkov <skwllsp@gmail.com>
2016-08-26 20:47:45 +0100

    setup

--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,8 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 config = {
     'description': 'read-through cache',
     'author': 'Sergei Kurenkov',
-    'url': 'https://github.com/skwllsp/read_through_cache',
-    'download_url': 'https://github.com/skwllsp/read_through_cache',
     'author_email': 'sergei.a.kurenkov@gmail.com',
     'version': '1.0',
     'install_requires': ['nose'],
@@ -15,5 +10,4 @@ config = {
     'scripts': [],
     'name': 'read_through_cache'
 }
-
 setup(**config)
