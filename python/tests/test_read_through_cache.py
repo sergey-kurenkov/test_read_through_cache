6288e58d7c3af15986fd31cb5bce1543933af3b6
Sergey.Kurenkov <skwllsp@gmail.com>
2016-08-27 20:05:36 +0100

    improved import

--- python/tests/test_read_through_cache.py
+++ python/tests/test_read_through_cache.py
@@ -3,8 +3,11 @@
 """ unittests for read through cache """
 
 import mock
-import read_through_cache
 import nose
+
+import read_through_cache.get_user_by_id
+from read_through_cache.cache import Cache, CacheException
+from read_through_cache.get_user_by_id import getUserByID
 
 def setup():
     """ setup """
@@ -16,8 +19,8 @@ def teardown():
 
 def test_get_user_by_id():
     """  simple getUserByID checks """
-    assert read_through_cache.get_user_by_id.getUserByID(1) == "UserName #1"
-    assert read_through_cache.get_user_by_id.getUserByID(2) == "UserName #2"
+    assert getUserByID(1) == "UserName #1"
+    assert getUserByID(2) == "UserName #2"
 
 def test_injected_get_user_by_id():
     """  simple injected function checks """
@@ -29,32 +32,32 @@ def test_injected_get_user_by_id():
 
 def test_get_names():
     """ get entries from a default cache """
-    cache = read_through_cache.cache.Cache()
-    assert cache.get_user_name(1) == read_through_cache.get_user_by_id.getUserByID(1)
-    assert cache.get_user_name(1) == read_through_cache.get_user_by_id.getUserByID(1)
-    assert cache.get_user_name(2) == read_through_cache.get_user_by_id.getUserByID(2)
-    assert cache.get_user_name(2) == read_through_cache.get_user_by_id.getUserByID(2)
+    cache = Cache()
+    assert cache.get_user_name(1) == getUserByID(1)
+    assert cache.get_user_name(1) == getUserByID(1)
+    assert cache.get_user_name(2) == getUserByID(2)
+    assert cache.get_user_name(2) == getUserByID(2)
 
-@nose.tools.raises(read_through_cache.cache.CacheException)
+@nose.tools.raises(CacheException)
 def test_bad_id():
     """ get entries from a default cache """
-    cache = read_through_cache.cache.Cache()
+    cache = Cache()
     cache.get_user_name(-1)
 
 def test_get_names_with_limit():
     """ get entries from a default cache with a limit on the number of entries """
-    cache = read_through_cache.cache.Cache(1)
-    assert cache.get_user_name(1) == read_through_cache.get_user_by_id.getUserByID(1)
-    assert cache.get_user_name(2) == read_through_cache.get_user_by_id.getUserByID(2)
-    assert cache.get_user_name(1) == read_through_cache.get_user_by_id.getUserByID(1)
-    assert cache.get_user_name(2) == read_through_cache.get_user_by_id.getUserByID(2)
+    cache = Cache(1)
+    assert cache.get_user_name(1) == getUserByID(1)
+    assert cache.get_user_name(2) == getUserByID(2)
+    assert cache.get_user_name(1) == getUserByID(1)
+    assert cache.get_user_name(2) == getUserByID(2)
 
 def test_with_injected_func():
     """ get entries from a test cache """
     with mock.patch('read_through_cache.get_user_by_id.getUserByID',
                     return_value='test_name') as injected_func:
         assert not injected_func.called
-        cache = read_through_cache.cache.Cache()
+        cache = Cache()
         assert cache.get_user_name(1) == "test_name"
         assert injected_func.call_count == 1
         assert cache.get_user_name(1) == "test_name"
@@ -73,7 +76,7 @@ def test_with_func_and_limit():
     with mock.patch('read_through_cache.get_user_by_id.getUserByID',
                     return_value='test_name') as injected_func:
         assert not injected_func.called
-        cache = read_through_cache.cache.Cache(1)
+        cache = Cache(1)
         assert cache.get_user_name(1) == "test_name"
         assert injected_func.call_count == 1
         assert cache.get_user_name(1) == "test_name"
@@ -84,4 +87,3 @@ def test_with_func_and_limit():
         assert injected_func.call_count == 2
         assert cache.get_user_name(1) == "test_name"
         assert injected_func.call_count == 3
-
